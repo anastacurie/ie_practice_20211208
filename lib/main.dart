c766d1ccac6800c75dae586ef99c8377ddebbe08
anastacurie <anastacurie@gmail.com>
2021-12-09 08:32:32 +0200

    Second Commit after adding SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -17,43 +17,56 @@ class MyApp extends StatelessWidget {
           Row(
             children: [
               Expanded(
-                child: Column(
-                  children: [
-                    Icon(Icons.arrow_back, size: 40),
-                    SideButton(the_icon: Icons.directions_bike),
-                    SideButton(the_icon: Icons.directions_car),
-                    SideButton(the_icon: Icons.directions_bus),
-                    SideButton(the_icon: Icons.directions_walk),
-                  ],
+                child: Container(
+                  height: size.height * 0.8,
+                  padding: EdgeInsets.only(top: size.height * 0.05),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Column(
+                      children: [
+                        const Icon(Icons.arrow_back, size: 40),
+                        SideButton(the_icon: Icons.directions_bike),
+                        SideButton(the_icon: Icons.directions_car),
+                        SideButton(the_icon: Icons.directions_bus),
+                        SideButton(the_icon: Icons.directions_walk),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-              Center(
-                child: Container(
-                  width: size.width * 0.75,
-                  height: size.height * 0.8,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40),
-                      topLeft: Radius.circular(40),
+              Container(
+                width: size.width * 0.75,
+                height: size.height * 0.8,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    topLeft: Radius.circular(40),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.lightBlue,
+                      offset: Offset(5, 5),
+                      blurRadius: 30,
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.lightBlue,
-                        offset: Offset(5, 5),
-                        blurRadius: 30,
-                      ),
-                    ],
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        "images/flower_in_vase1.png",
-                      ),
+                  ],
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      "images/flower_in_vase1.png",
                     ),
                   ),
                 ),
               ),
             ],
-          )
+          ),
+          Row(children: [
+            RichText(
+              text: TextSpan(text: "Rose\n", children: [
+                TextSpan(text: "Rwanda"),
+              ]),
+            ),
+            Text("RF20,000")
+          ])
         ],
       ),
     );
